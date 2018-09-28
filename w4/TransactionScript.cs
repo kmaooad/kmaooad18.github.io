f273ebbfb4246b1bbcfd1ecb999e1ef77947b7de
Anton Shabinskiy <a@shbk.me>
2018-09-28 09:02:40 +0300

    Update TransactionScript.cs

--- w4/TransactionScript.cs
+++ w4/TransactionScript.cs
@@ -8,7 +8,17 @@ namespace KmaOoad18.Samples.Week4
             {
                 var order = db.Orders.Single(c => c.Id = orderId);
 
-
+                foreach (var orderItem in order.Items)
+                {
+                    var discount = db.Discounts.Where(d => d.Product.Id == orderItem.Product.Id).ToList();
+                    
+                    foreach(var discount in discounts)
+                    {
+                        orderItem.Total = orderItem.Qty * orderItem.Product.Price * (1 - discount.Value / 100);
+                    }
+                }
+                
+                db.SaveChanges();
             }
         }
     }
