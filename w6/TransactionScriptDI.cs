7197720d74994cf3a82a661c4f5ddbee0142a0cb
Anton Shabinskiy <a@shbk.me>
2018-10-12 08:46:36 +0300

    Update TransactionScriptDI.cs

--- w6/TransactionScriptDI.cs
+++ w6/TransactionScriptDI.cs
@@ -2,27 +2,28 @@ namespace KmaOoad18.Samples.Week6
 {
     public class Discounter
     {
-		private readonly DiscountContext _db;
+	private readonly DiscountContext _db;
 		
-		public Discounter(DiscountContext db){
-			_db = db;
-		}
+	public Discounter(DiscountContext db)
+	{
+	    _db = db;
+	}
 		
         public void ApplyDiscount(int orderId)
         {
-			var order = _db.Orders.Single(c => c.Id == orderId);
+            var order = _db.Orders.Single(c => c.Id == orderId);
 
-			foreach (var orderItem in order.Items)
-			{
-				var discount = _db.Discounts.Where(d => d.Product.Id == orderItem.Product.Id).ToList();
-				
-				foreach(var discount in discounts)
-				{
-					orderItem.Total = orderItem.Qty * orderItem.Product.Price * (1 - discount.Value / 100);
-				}
-			}
-			
-			_db.SaveChanges();
+            foreach (var orderItem in order.Items)
+	    {
+		    var discount = _db.Discounts.Where(d => d.Product.Id == orderItem.Product.Id).ToList();
+		    
+		    foreach(var discount in discounts)
+		    {
+			    orderItem.Total = orderItem.Qty * orderItem.Product.Price * (1 - discount.Value / 100);
+		    }
+	    }
+	
+	    _db.SaveChanges();
         }
     }
 
